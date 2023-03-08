3b4544a9318cb8bf953084479ffc48bdff0ea866
Junayd H <junaydhus@gmail.com>
2023-03-08 14:19:52 -0800

    render panels and add onclick function to switch focus

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,19 +2,65 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 
 class Dashboard extends Component {
   state = {
-    loading: true
+    loading: false,
+    focused: null
   };
 
-  render() {
-    const dashboardClasses = classnames("dashboard");
-
-  if (this.state.loading) {
-    return <Loading />;
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
   }
-    return <main className={dashboardClasses} />;
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map(panel => (
+     <Panel
+      key={panel.id}
+      id={panel.id}
+      label={panel.label}
+      value={panel.value}
+      onSelect={event => this.selectPanel(panel.id)}
+     />
+    ));
+    
+    return <main className={dashboardClasses}>{panels}</main>
   }
 }
 
